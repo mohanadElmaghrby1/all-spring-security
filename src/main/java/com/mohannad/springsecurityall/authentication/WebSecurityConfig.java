a45688785001c109620777dcb6233fe55b7f2718
Mohannad <mohanad2021996@gmail.com>
2020-04-22 15:11:39 +0200

    In memory Auth

--- src/main/java/com/mohannad/springsecurityall/authentication/WebSecurityConfig.java
+++ src/main/java/com/mohannad/springsecurityall/authentication/WebSecurityConfig.java
@@ -4,12 +4,32 @@ package com.mohannad.springsecurityall.authentication;
  * @author Mohannad Elmagharby
  * on 4/22/2020
  */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("test")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
