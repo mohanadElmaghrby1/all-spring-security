3c30e55cd55714f63d228f05a853c943c4e82834
Mohannad <mohanad2021996@gmail.com>
2020-04-22 11:57:52 +0200

    project setup

---
+++ src/test/java/com/mohannad/springsecurityall/SpringSecurityAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohannad.springsecurityall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
